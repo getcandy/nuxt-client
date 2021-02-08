e92fd35b7068fff1fe12c8ddce6dec88a2873d60
Alec <hello@itsalec.co.uk>
2021-02-08 10:19:01 +0000

    Refactor nuxt client

--- src/module.js
+++ src/module.js
@@ -1,18 +1,6 @@
 const path = require('path')
 
 module.exports = function (moduleOptions) {
-    const templates = [
-        // Store
-        './HttpClient.js',
-      ];
-
-      templates.forEach((template) => {
-        this.addTemplate({
-          src: path.resolve(__dirname, template),
-          fileName: template
-        })
-      })
-
     this.addPlugin({
         src: path.resolve(__dirname, 'plugin.js'),
         options: moduleOptions
--- src/plugin.js
+++ src/plugin.js
@@ -1,7 +1,16 @@
-import HttpClient from './HttpClient'
+const kebabCase = require('lodash/kebabCase')
+const camelCase = require('lodash/camelCase')
+const capitalize = require('lodash/capitalize')
 
-export default ({ app }, inject) => {
-    const options = <%= JSON.stringify(options, null, 2) %>
+export default ({ app, $axios }, inject) => {
+    inject('getcandy', {
+      async on (ref, method, ...opts) {
+        const moduleRef = `${capitalize(camelCase(ref))}Api`
+        const apiRef = `${kebabCase(moduleRef)}`
+        const apiModule = await import(`@getcandy/js-client/dist/api/${apiRef}.js`)
+        const module = new apiModule[moduleRef](null, '', app.$auth.ctx.$axios)
+        return module[method](...opts)
+      }
+    })
 
-    inject('getcandy', new HttpClient)
 }
