5a37efea139228334827f95cae61b92fa9231ca5
Alec <hello@itsalec.co.uk>
2021-02-17 08:06:57 +0000

    Init 0.12.0

--- src/plugin.js
+++ src/plugin.js
@@ -1,15 +1,20 @@
 const kebabCase = require('lodash/kebabCase')
 const camelCase = require('lodash/camelCase')
-const capitalize = require('lodash/capitalize')
+const upperFirst = require('lodash/upperFirst')
 
 export default ({ app, $axios }, inject) => {
     inject('getcandy', {
       async on (ref, method, ...opts) {
-        const moduleRef = `${capitalize(camelCase(ref))}Api`
+        const moduleRef = `${upperFirst(camelCase(ref))}Api`
         const apiRef = `${kebabCase(moduleRef)}`
-        const apiModule = await import(`@getcandy/js-client/dist/api/${apiRef}.js`)
-        const module = new apiModule[moduleRef](null, '', app.$auth.ctx.$axios)
-        return module[method](...opts)
+        try {
+          const apiModule = await import(`@getcandy/js-client/dist/api/${apiRef}.js`)
+          const className = apiModule[moduleRef]
+          const module = new className(null, '', app.$auth.ctx.$axios)
+          return module[method](...opts)
+        } catch (e) {
+          throw new Error(`Unable to find ${method} on ${apiRef}`)
+        }
       }
     })
 
